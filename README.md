03cad819f246e4a2debef20b73aa1cc39931a75c
James Skön <skonjp@kenyon.edu>
2024-01-30 10:07:41 -0500

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # SD-EnumerableMethods
 This is an activity to learn about Enumerable Methods in Ruby
+
+1. Clone the project on your computer
+1. Examine the code and run it as is.  
+1. How can we simplify and improve the code?
+1. Finish the function `getFile` to break the file into words without punctuation.  Use `map` to apply the gsub and downcase.
+1. Consider if we wish to "filter" our count based on word types (see the word type files `verb.txt` etc.)  Complete `createCountTableFilteredSimple`
+1. How can we improve `createCountTableFilteredSimple` using predicate selectors? Rewrite in `createCountTableFilteredShort`
+1. Review `verbsConjugated.txt`.  How can we count verb unifying the counts based on verb tense?
+1. Complete the code `readVerbs`to create a hash where each verb for points to present tense version
+# e.g. {"scold"=>"scold","scolds"=>"scold","scolding"=>"scold","scolded"=>"scold","scolded"=>"scold"}
+1. Now complete the code `createCountVerbs` to crearte a table of counts using the hash from above to unify the counts.
+
---
+++ WordExplore.rb
@@ -0,0 +1,86 @@
+# Program to explore text by word types.
+# James Skon, Kenyon College
+# January 2024
+
+# Read file and break into lines
+def getFileLines(name)
+    fileObj = File.open(name)
+    text=fileObj.read()
+    text.split("\n")
+end
+
+# Read file and break into words, remove special character, downcase
+def getFile(name)
+
+end
+
+# Read in Conjugated verbs into a hash where each verb for points to present tense version
+# e.g. {"scold"=>"scold","scolds"=>"scold","scolding"=>"scold","scolded"=>"scold","scolded"=>"scold"}
+def readVerbs
+
+end
+
+# Create count table of text count hash counting words from whitelist 
+def createCountTableFilteredSimple(lines,whitelist) 
+    table = {}
+    text.each do |line|
+        words = line.split(" ")
+        words.each do |word|
+            puts word
+            word=word.gsub(/\W/, '').downcase.chomp
+            if whitelist.include?(word)
+                if table.include?(word)
+                    table[word]+=1
+                else
+                    table[word]=1
+                end
+            end
+        end
+    end
+    table.sort_by {|key,value| value}    
+end
+
+# Create count table of text count hash counting words from whitelist 
+# Use selction predicates
+def createCountTableFilteredShort(text,whitelist) 
+   
+end
+
+# Create a count of verbs, combining all forms of verbs into the root verb (present tense)
+def createCountVerbs(verbHash,words)
+    verbCount = {}
+    words.each do |w|
+        if verbHash.include?(w)
+            #puts "#{w}, #{verbHash[w]}, #{verbCount[w]}, #{verbCount[verbHash[w]]}"
+            if !verbCount.include?(verbHash[w])
+                puts "#{verbHash[w]} not in  verbCount"
+                verbCount[verbHash[w]]=0
+            end
+            if verbCount.include?(w)
+                print "<"
+                verbCount[verbHash[w]]+=1
+            else
+                print ">"
+                verbCount[verbHash[w]]=1
+            end
+        end
+    end
+    verbCount.sort_by {|key,value| value}
+end
+
+def displayTable(aHash)
+    aHash.each do |key,value|
+        print "#{key}: #{value}\n"
+    end
+end
+
+words = getFile("adjectives.txt")
+text = getFile("emilydickenson.txt")
+
+#countTable=createCountTableFilteredShort(text,words)
+#displayTable(countTable)
+
+#verbHash = readVerbs()
+#displayTable(verbHash)
+#verbCount=createCountVerbs(verbHash,text)
+#displayTable(verbCount)
